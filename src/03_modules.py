381d58f1a0c262c2069f7e138f0ec30eb9ff2294
David-E-Alvarez <davidealvarez1@gmail.com>
2020-07-06 21:31:06 -0700

    couldn't figure out finding machine's login name

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,22 +10,28 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+#1.)print("sys.argv: ", sys.argv)#prints "sys.argv:  ['03_modules.py']"
+#2.)print("sys.argv[1]: ", sys.argv[1])#prints "sys.argv[1]:  LambdaRules!" if "python3 03_modules.py LambdaRules!" in terminal
+#3.)print("2 + 2: ", int(sys.argv[1]) + int(sys.argv[2]))# "python3 03_modules.py 2 2"
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+#print(sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
+#print(sys.version)
 
-
-import os
+import os, pwd
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
-
 # Print the current process ID
 # YOUR CODE HERE
+#print("current process id: ", os.getgid())
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+#print("current working directory: ", os.getcwd())
 
 # Print out your machine's login name
 # YOUR CODE HERE
+#print(pwd.getpwuid(os.getuid())[0])
