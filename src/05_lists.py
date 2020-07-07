da4d3f267b351f3a87c2736d5575c891b6e349ee
David-E-Alvarez <davidealvarez1@gmail.com>
2020-07-07 13:22:16 -0700

    runs on yoga

--- src/05_lists.py
+++ src/05_lists.py
@@ -9,35 +9,35 @@ y = [8, 9, 10]
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
 x.append(4)
-print(x)
+#print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
-print(x)
+#print(x)
 #x.append(y) i don't like the output of this on repl.it but don't know if i can do print(x + y)
-print(x + y)
+#print(x + y)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
-print(x + y[1:])
+#print(x + y[1:])
 #or x.append(y[1:]) to use print(x)
-print(x)
+#print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 #for this one the first thing i do is add the 99 like so:
-y.insert(2,99)
+#y.insert(2,99)
 #i then can append y to x and "splice" the 8 out like so:
-x.append(y[1:])
+#x.append(y[1:])
 #if i do the line above i can use print(x) but if i can also do this:
-print(x + y[1:])#and not do the appending of y to x
+#print(x + y[1:])#and not do the appending of y to x
 # YOUR CODE HERE
-print(x)
+#print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
-print(len(x))
+#print(len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
-for elem in x:
-  print(elem * 1000)
+# for elem in x:
+#   print(elem * 1000)
