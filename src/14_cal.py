2d9b4c0292597884a5ad2338e98114b0efa71ac1
David-E-Alvarez <davidealvarez1@gmail.com>
2020-07-08 17:55:13 -0700

    14_cal done

--- src/14_cal.py
+++ src/14_cal.py
@@ -6,15 +6,15 @@ https://docs.python.org/3.6/library/calendar.html
 Write a program that accepts user input of the form
   `14_cal.py [month] [year]`
 and does the following:
- - If the user doesn't specify any input, your program should
+ - [x]If the user doesn't specify any input, your program should
    print the calendar for the current month. The 'datetime'
    module may be helpful for this.
- - If the user specifies one argument, assume they passed in a
+ - [x]If the user specifies one argument, assume they passed in a
    month and render the calendar for that month of the current year.
- - If the user specifies two arguments, assume they passed in
+ - [x]If the user specifies two arguments, assume they passed in
    both the month and the year. Render the calendar for that
    month and year.
- - Otherwise, print a usage statement to the terminal indicating
+ - []Otherwise, print a usage statement to the terminal indicating
    the format that your program expects arguments to be given.
    Then exit the program.
 
@@ -30,3 +30,38 @@ it should use today’s date to get the month and year.
 import sys
 import calendar
 from datetime import datetime
+
+
+#this program runs in terminal. it takes in two arguments but user doesn't have to enter any. 
+# EX. `14_cal.py [month] [year]`
+# print('sys.argv[1]: ', sys.argv[1])# would print what user would enter for month
+#print(len(sys.argv) - 1)#can use to determine how many arguments user enters
+
+#calendar for a month and year
+cal = calendar.TextCalendar()
+current_month = datetime.now().month
+current_year = datetime.now().year
+
+user_args_length = len(sys.argv)-1 #number of user arguments
+#print('--->',user_args_length)
+
+#1.)if user enters nothing output calendar for current month
+if user_args_length == 0:
+  print(cal.prmonth(current_year,current_month))
+
+#2.) if user enters one argument; assume month is entered
+elif user_args_length == 1:
+  year = datetime.now().year
+  month = int(sys.argv[1])
+  print(cal.prmonth(year,month))
+
+#3.) if user enters two arguments output month and year they want
+elif user_args_length == 2:
+  year = datetime.now().year
+  month = int(sys.argv[1])
+  print(cal.prmonth(year,month))
+else:
+  # print a usage statement 
+    print("usage: 14_cal.py [month] [year]")
+    # exit the program 
+    sys.exit(1)
